c0900fe9b7de927fff4844f1c129db1aec74099e
Sherwin <sherwin.chiu@gmail.com>
2021-01-17 03:57:42 +0000

    s

--- mask.py
+++ mask.py
@@ -10,7 +10,7 @@ def detectMask(imagePath):
     np.set_printoptions(suppress=True)
 
     # Load the model
-    model = model.save("keras_model.h5")
+    model = keras.models.load_model("keras_model.h5")
     
     # Create the array of the right shape to feed into the keras model
     # The 'length' or number of images you can put into the array is
