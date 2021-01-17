7335ab89b196d5ecd708b77fe6646d4633573d8d
Matthew <matt.lqh02@gmail.com>
2021-01-16 19:15:49 -0500

    Updated with main()

--- face.py
+++ face.py
@@ -1,27 +1,32 @@
 # face.py
-# Sherwin Chiu, Kyrollous Nassif, George Li, Matthew Hao 
+# Sherwin Chiu, Kyrollous Nassif, George Li, Matthew Hao
 # 01/16/2021
 # Desc: Gets an image and compares it to a cascade which can classify whether or not something is a face already.
 # Detects the face and returns how many faces have been detected
 import sys
 import cv2
 
-def detectFace():
+
+def detectFace(imagePath):
     cv2.setUseOptimized(True)
     # Getting paths for cascade/image
-    imagePath = "test-data/8-with-mask.jpg"
+
+    # #imagePath = "test-data/8-with-mask.jpg"
     cascadePath = "haarcascade_frontalface_default.xml"
     # Process image and cascades to classify them
     image = cv2.imread(imagePath)
     cascade = cv2.CascadeClassifier(cascadePath)
     # Compare images to cascade, detect how many faces are there
-    faces = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,minSize=(50,50))
+    faces = cascade.detectMultiScale(
+        image, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50)
+    )
     # Show faces (for testing)
     for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 1)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
     cv2.imshow("test", image)
     cv2.waitKey(0)
     # Return number of faces
     return len(faces)
 
-print(detectFace())
+
+# print(detectFace())
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import face
 import camera
-print("hello")
+import mask
 
 
-print("raspberry pi commit")
+def main():
+    print("Face Mask Detection System V1.0/nThank you for using")
+
+    while True:
+        camera.takePicture(10)
+
+        if face.detectFace("/home/pi/Desktop/mask-detector/p.jpg") > 0:
+            # face detected, check if mask
+
+            if mask.detectMask("/home/pi/Desktop/mask-detector/p.jpg"):
+                print("Mask Detected, thank you")
+            else:
+                print("Mask not found, please put a mask on before entering")
+
+        camera.deletePicture()
+
+
+if __name__ == "__main__":
+    main()
