065d2543cb936c7fff0186e8b191174237c892c4
Sherwin <sherwin.chiu89@gmail.com>
2021-01-17 00:20:33 -0500

    Client.py finished

--- client/client.py
+++ client/client.py
@@ -1,15 +1,19 @@
 from __future__ import print_function
-import requestsimport json
+import requests
 import json
 import cv2
 
-addr = 'http:ip:5000'
+ip = '174.119.148.14'
+port = '25565'
+addr = 'http://'+ip+':'+port
+
 test_url = addr + '/api/test'
 
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
 img = cv2.imread('lena.jpg')
-_, img_encoded = cv2.imencode('.jpg',img)
+img_encoded = cv2.imencode('.jpg',img)
 response = requests.post(test_url, data=img_encoded.tostring(),headers=headers)
+
 print(json.loads(response.text))
