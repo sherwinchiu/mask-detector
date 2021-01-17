25380e212f131a2de1b4ebc4a2d167805bbab79b
Sherwin <sherwin.chiu@gmail.com>
2021-01-17 07:46:28 +0000

    client.py updated

--- client/client.py
+++ client/client.py
@@ -3,17 +3,17 @@ import requests
 import json
 import cv2
 
-ip = '174.119.148.14'
-port = '25565'
+ip = '2.tcp.ngrok.io'
+port = '18342'
 addr = 'http://'+ip+':'+port
 
 test_url = addr + '/api/test'
 
-content_type = 'image/jpeg'
+content_type = "image/jpeg"
 headers = {'content-type': content_type}
 
-img = cv2.imread('lena.jpg')
-img_encoded = cv2.imencode('.jpg',img)
+img = cv2.imread('/home/pi/Desktop/mask-detector/p.jpeg')
+_, img_encoded = cv2.imencode('.jpg',img)
 response = requests.post(test_url, data=img_encoded.tostring(),headers=headers)
 
 print(json.loads(response.text))
